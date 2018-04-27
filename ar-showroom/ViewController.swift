f2b5d9068e1e0466d81096bfbc7482082ffaa14f
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2018-04-27 17:24:55 -0400

    display of tesla model s in vr

--- ar-showroom/ViewController.swift
+++ ar-showroom/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var nodeModel:SCNNode!
+    let nodeName = "tesla"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,13 +24,26 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        sceneView.showsStatistics = false
+        
+        //Enable multisampling
+        sceneView.antialiasingMode = .multisampling4X
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        //enable scene lighting to illuminate nodes
+        sceneView.automaticallyUpdatesLighting = true
+        sceneView.autoenablesDefaultLighting = true
+        
+        //Create scene from model
+        let modelScene = SCNScene(named: "art.scnassets/tesla/tesla.dae")!
+        
+        //add all nodes from root of the model
+        nodeModel = modelScene.rootNode.childNode(withName: nodeName, recursively: true)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -54,14 +70,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if !anchor.isKind(of: ARPlaneAnchor.self) {
+            DispatchQueue.main.async {
+                let modelClone = self.nodeModel.clone()
+                modelClone.position = SCNVector3Zero
+                
+                // Add model as a child of the node
+                node.addChildNode(modelClone)
+            }
+        }
     }
-*/
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
@@ -76,5 +95,47 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+    
+    func getParent(_ nodeFound: SCNNode?) -> SCNNode? {
+        if let node = nodeFound {
+            if node.name == nodeName {
+                return node
+            } else if let parent = node.parent {
+                return getParent(parent)
+            }
+        }
+        return nil
+    }
+    
+    //Add or Remove Node from the enviroment
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let location = touches.first!.location(in: sceneView)
+        
+        // node was touched, remove from scene
+        var hitTestOptions = [SCNHitTestOption: Any]()
+        hitTestOptions[SCNHitTestOption.boundingBoxOnly] = true
+        
+        let hitResults: [SCNHitTestResult]  = sceneView.hitTest(location, options: hitTestOptions)
+        
+        if let hit = hitResults.first {
+            if let node = getParent(hit.node) {
+                node.removeFromParentNode()
+                return
+            }
+        }
+        
+        // Node wasn't touched, find feature points to place the node
+        let hitResultsFeaturePoints: [ARHitTestResult]  = sceneView.hitTest(location, types: .featurePoint)
+        
+        if let hit = hitResultsFeaturePoints.first {
+            
+            // Get the rotation matrix of the camera
+            let rotate = simd_float4x4(SCNMatrix4MakeRotation(sceneView.session.currentFrame!.camera.eulerAngles.y, 0, 1, 0))
+            
+            // Combine the matrices
+            let finalTransform = simd_mul(hit.worldTransform, rotate)
+            sceneView.session.add(anchor: ARAnchor(transform: finalTransform))
+        }
     }
 }
